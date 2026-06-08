0d88122a734e43fa84d73675538e75a2761679de
Konrad Weiss <konrad.weiss@aisec.fraunhofer.de>
2026-06-08 11:48:28 +0200

    Updating build script for rust

--- rustast/build.sh
+++ rustast/build.sh
@@ -1,25 +1,38 @@
 #!/bin/bash
 set -e
 
-cargo build --release
-
 OS=$(uname -s | tr '[:upper:]' '[:lower:]')
 ARCH=$(uname -m)
 
-case "$OS" in
-    linux)
-        EXT=so
-        ;;
-    darwin)
-        EXT=dylib
-        ;;
-    mingw*|msys*)
-        EXT=dll
-        ;;
-    *)
-        echo "Unsupported OS"
-        exit 1
-        ;;
-esac
+echo "Building on $OS for $ARCH"
 
-echo "Built target/release/libcpgrust.${EXT}"
+if [ "$OS" = "darwin" ]; then
+    cargo build --release --target aarch64-apple-darwin
+    cargo build --release --target x86_64-apple-darwin
+
+    cp target/aarch64-apple-darwin/release/libcpgrust.dylib ./libcpgrust-arm64.dylib
+    cp target/x86_64-apple-darwin/release/libcpgrust.dylib ./libcpgrust-amd64.dylib
+
+elif [ "$OS" = "linux" ]; then
+    if [ "$ARCH" = "aarch64" ]; then
+        cargo build --release
+        cp target/release/libcpgrust.so ./libcpgrust-arm64.so
+    elif [ "$ARCH" = "x86_64" ]; then
+        cargo build --release
+        cp target/release/libcpgrust.so ./libcpgrust-amd64.so
+    else
+        echo "Unsupported Linux architecture: $ARCH"
+        exit 1
+    fi
+
+elif [[ "$OS" == mingw64* ]] || [[ "$OS" == msys* ]]; then
+    cargo build --release
+    cp target/release/cpgrust.dll ./libcpgrust-amd64.dll
+
+else
+    echo "Unsupported OS"
+    echo "Current OS: $OS"
+    exit 1
+fi
+
+echo "Build complete"
