aecf59b6e0bc6cd14dce7ebcf0fb4c0a25b79909
Konrad Weiss <konrad.weiss@aisec.fraunhofer.de>
2026-06-08 21:55:57 +0200

    Added rust AST library (#9)
    * Add rust library and build file
    
    * Updating build script for rust
    
    * Integrate Rust into build
    
    * Use different rust setup action
    
    * Fixing and optimizing build on mac runners
    
    * Running fmt
    
    * change package name to rustast

---
+++ rustast/build.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+set -e
+
+OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+ARCH=$(uname -m)
+
+echo "Building on $OS for $ARCH"
+
+if [ "$OS" = "darwin" ]; then
+    if [ "$ARCH" = "arm64" ]; then
+        cargo build --release
+        cp target/release/librustast.dylib ./librustast-arm64.dylib
+
+    elif [ "$ARCH" = "x86_64" ]; then
+        cargo build --release
+        cp target/release/librustast.dylib ./librustast-amd64.dylib
+
+    else
+        echo "Unsupported macOS architecture: $ARCH"
+        exit 1
+    fi
+
+elif [ "$OS" = "linux" ]; then
+    if [ "$ARCH" = "aarch64" ]; then
+        cargo build --release
+        cp target/release/librustast.so ./librustast-arm64.so
+    elif [ "$ARCH" = "x86_64" ]; then
+        cargo build --release
+        cp target/release/librustast.so ./librustast-amd64.so
+    else
+        echo "Unsupported Linux architecture: $ARCH"
+        exit 1
+    fi
+
+elif [[ "$OS" == mingw64* ]] || [[ "$OS" == msys* ]]; then
+    cargo build --release
+    cp target/release/rustast.dll ./librustast-amd64.dll
+
+else
+    echo "Unsupported OS"
+    echo "Current OS: $OS"
+    exit 1
+fi
+
+echo "Build complete"
